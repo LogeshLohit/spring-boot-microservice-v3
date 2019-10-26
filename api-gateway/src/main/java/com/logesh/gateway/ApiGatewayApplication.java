0aec4deab0cc833fc37b58137e0b86f85e800c93
Logesh <logesh.jeppiaar@gmail.com>
2019-10-26 15:51:01 +0530

    zuul - api gateway - router

---
+++ api-gateway/src/main/java/com/logesh/gateway/ApiGatewayApplication.java
@@ -0,0 +1,17 @@
+package com.logesh.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@CrossOrigin
+@EnableZuulProxy
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+}
